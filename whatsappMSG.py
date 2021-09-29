a95032932e90106638c1e4082fe5b6ba035fe265
tharunkarnekota <Tharunkarnekota@gmail.com>
2021-09-29 22:23:42 +0530

    whatsapp messenger with time

---
+++ whatsappMSG.py
@@ -0,0 +1,13 @@
+import pywhatkit
+
+#when we are designing a app for school
+#when we want to sent fee due message to parents by rntering there mobile number
+#it automatically sents the message
+
+mobno = input('enter whatsapp mobile number: ')
+msg = input('message: ')
+hr = int(input('hours(24 hours format): '))
+mint = int(input('minutes: '))
+pywhatkit.sendwhatmsg('+91'+ mobno, msg, hr, mint)
+
+#pywhatkit.sendwhatmsg('+916303542909', 'hello', 18, 7)
