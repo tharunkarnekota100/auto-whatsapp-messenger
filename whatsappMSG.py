12d8fc9e2c416e8ae776c43f330a44ebe96e8c1c
tharunkarnekota100 <91623178+tharunkarnekota100@users.noreply.github.com>
2021-09-29 22:25:32 +0530

    Update whatsappMSG.py

--- whatsappMSG.py
+++ whatsappMSG.py
@@ -1,7 +1,7 @@
 import pywhatkit
 
 #when we are designing a app for school
-#when we want to sent fee due message to parents by rntering there mobile number
+#when we want to sent fee due message to parents by entering there mobile number
 #it automatically sents the message
 
 mobno = input('enter whatsapp mobile number: ')
